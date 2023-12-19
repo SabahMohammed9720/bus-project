e2e76b4f82f8c706dccd316f92c4519ff4debcd5
ahmed.ghazaly <ahmedghazaly2015@gmail.com>
2023-12-19 23:55:55 +0200

    commit - hagar Comments - nav bar to js

--- static/js/main.js
+++ static/js/main.js
@@ -1,35 +1,37 @@
 function showNavbar(){
     document.write( `
+    <!--         Nav_Bar         -->
     <div class="header" id="fixed">
-        <div class="logo">
-            <img src="images/logo.png" alt="">
-            <p>BUS FINDER</p>
+      <div class="logo">
+        <img src="/static/pics/logo.png" alt="error" />
+        <p>BUS FINDER</p>
+      </div>
+      <div class="right-side">
+        <div class="navbar">
+          <ul>
+            <li>
+              <a href="home">HOME</a>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <a href="about">ABOUT</a>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <a href="add" id="services">ADD</a>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <a href="footer">CONTACT US</a>
+            </li>
+          </ul>
         </div>
-        <div class="right-side">
-            <div class="navbar">
-                <ul>
-                    <li>
-                        <a href="land.html" id="home">HOME</a>
-                    </li>
-                </ul>
-                <ul>
-                    <li>
-                        <a href="about.html" id="about">ABOUT</a>
-                    </li>
-                </ul>
-                <ul>
-                    <li>
-                        <a href="search.html" id="services">SERVICES</a>
-                    </li>
-                </ul>
-                <ul>
-                    <li>
-                        <a href="footer.html" id="contact">CONTACT US</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
+      </div>
     </div>
+    <!--         Nav_Bar         -->
         `
     )
 }
